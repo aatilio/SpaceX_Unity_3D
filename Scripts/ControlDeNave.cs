c9fb738ae820900f220ab084e031567d13c4eef2
ahoraraho <arahogrou@gmail.com>
2022-10-28 10:54:59 -0500

    refactorisacion de la nave

--- Scripts/ControlDeNave.cs
+++ Scripts/ControlDeNave.cs
@@ -25,10 +25,18 @@ public class ControlDeNave : MonoBehaviour
     //metodo privado que sirbe para saber que tecla se esta pulsando
     private void ProcesarImput()
     {
-        //esta condicion nos dice que tecla es la que esta siendo pulsada
+        Propulsion();
+        RotacionDerecheIzquierda();
+        RotacionAdelanteAtras();
+        TraslacionDerecheIzquierda();
+    }
+
+    public void Propulsion(){
+         //esta condicion nos dice que tecla es la que esta siendo pulsada
         if (Input.GetKey(KeyCode.Space))
         {
             //print("Propulsor de la Nave...");
+            rigidbody.freezeRotation = true;
             GetComponent<Rigidbody>().AddRelativeForce(Vector3.up);
             if (!audioSource.isPlaying)
             {
@@ -36,22 +44,29 @@ public class ControlDeNave : MonoBehaviour
             }
         } else {
             audioSource.Stop();
-        }
+        } 
+        rigidbody.freezeRotation = false;
+    }
+
+    public void RotacionDerecheIzquierda(){
         //ROTACION DERECHA IZQUIERDA
         if (Input.GetKey(KeyCode.Q))
         {
             var rotarIzquierda = transform.rotation;
             rotarIzquierda.x -= Time.deltaTime * 1;
             transform.rotation = rotarIzquierda;
-        }
+        }  
         else if (Input.GetKey(KeyCode.E))
         {
             var rotarDerecha = transform.rotation;
             rotarDerecha.x += Time.deltaTime * 1;
             transform.rotation = rotarDerecha;
         }
+    }
+
+    public void RotacionAdelanteAtras(){
         //ROTAR HACIA ADELANTE Y ATRAS
-        else if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
             var rotarAdelante = transform.rotation;
             rotarAdelante.z += Time.deltaTime * 1;
@@ -63,19 +78,22 @@ public class ControlDeNave : MonoBehaviour
             rotarAtras.z -= Time.deltaTime * 1;
             transform.rotation = rotarAtras;
         }
+    }
+
+    public void TraslacionDerecheIzquierda(){
         //TRASLACION DERECHA IZQUIERDA
-        else if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             var positionIzquierda = transform.position;
-            positionIzquierda.x -= Time.deltaTime * 10;
+            positionIzquierda.x -= Time.deltaTime * 3;
             transform.position = positionIzquierda;
         }
         else if (Input.GetKey(KeyCode.D))
         {
             var positionDerecha = transform.position;
-            positionDerecha.x += Time.deltaTime * 10;
+            positionDerecha.x += Time.deltaTime * 3;
             transform.position = positionDerecha;
-
         }
     }
+
 }
